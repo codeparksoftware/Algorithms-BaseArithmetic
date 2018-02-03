618fe2a562344e24bd924c485fc27ef2d5ee5489
codeparksoftware <kodpark@hotmail.com>
2018-02-03 16:49:01 +0200

    XBased Converter
    This source code is free to copy and use.It was  developed by c# . It contains  string to decimal and decimal to string converter functions on XBased. Also if you need to use custom character set you may use and create your custom set.(a-Z,0-9 etc.)
    Length was defined to specify length of digits.
    I paid attention to the algorithm's quick operation.
    There is some improvements.

---
+++ XBased.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForce
+{
+    public class XBased
+    {
+
+        #region Properties
+        private char[] _CharSet;
+        public char[] CharSet { get { return _CharSet; } set { _CharSet = value; } }
+
+        private short _length;
+        public short Length
+        {
+            get { return _length; }
+            set
+            {
+                _length = value;
+
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public XBased(short Length, char[] CharSet)
+        {
+            this._CharSet = CharSet;
+            this.Length = Length;
+
+        }
+        #endregion
+
+        #region ConverterMethods
+        public string ToXBased(BigInteger value)
+        {
+            //draft for N length X Based Value for example AAAAA
+            char[] array = Enumerable.Repeat(_CharSet[0], _length).ToArray();
+
+            int divider = _CharSet.Length;
+            short count = _length; //used to speedup making string 
+            do
+            {
+                count--;
+                array[count] = _CharSet[(Int64)(value % divider)];
+                value /= divider;
+
+            } while (value > 0);
+
+            return new string(array);
+        }
+        public BigInteger FromXBased(string str)
+        {
+            BigInteger big = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                big += Array.FindIndex(_CharSet, w => w.Equals(str[i])) * BigInteger.Pow(_CharSet.Length, (str.Length - i - 1));
+            }
+            return big;
+        }
+
+        #endregion
+    }
+}
